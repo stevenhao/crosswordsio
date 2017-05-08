b0d5f2c463031052c53a5a91cb056d7667af85d6
stevenhao <stevenhao@gmail.com>
2017-05-07 23:38:25 -0700

    compose wip

--- client/react/compose/index.js
+++ client/react/compose/index.js
@@ -7,41 +7,72 @@ export default class Compose extends Component {
   constructor() {
     super();
     this.state = {
-      clues: {
-        across: [],
-        down: [],
-      },
-      grid: [[{
-        black: false,
-        number: 1,
-        edits: [],
-        value: '',
-        parents: {
-          across: 1,
-          down: 1
-        }
-      }]],
+      size : 5
     };
   }
 
-  updateGrid(r, c, value) {
-    const grid = JSON.parse(JSON.stringify(this.state.grid));
-    grid[r][c].value = value;
-    this.setState({
-      grid: grid
-    });
+  listPuzzles() {
+    return [
+      {
+        pid: 5,
+        info: {
+          title: 'abc',
+          autho: 'steve'
+        },
+        dims: {
+          rows: 5,
+          cols: 5
+        }
+      }
+    ];
   }
 
   render() {
-    const size = 35 * 15 / this.state.grid.length;
+    const size = 35 * 15 / this.state.size;
     return (
-      <Game
-        size={size}
-        grid={this.state.grid}
-        clues={this.state.clues}
-        frozen={false}
-        updateGrid={this.updateGrid.bind(this)}
-      />
+      <div className='compose'>
+        <div className='compose--left'>
+          <h2> Your Puzzles </h2>
+          <div className='compose--left--list'>
+            {
+              this.listPuzzles.slice().map((entry, i) =>
+                <div
+                  key={i}
+                  onClick={this.selectPuzzle.bind(this, entry.pid)}
+                >
+                  <div className='welcome--browse--puzzlelist--entry'>
+                    <div>
+                      { entry.info.title } ({ entry.dims.rows } x { entry.dims.cols })
+                    </div>
+                  </div>
+                </div>
+              )
+            }
+          </div>
+        </div>
+        <div className='compose--main'>
+          <h2> Main content </h2>
+          <div className='compose--main--info'>
+            <div className='compose--main--info--pid'>
+            </div>
+            <div className='compose--main--info--title'>
+            </div>
+            <div className='compose--main--info--title'>
+            </div>
+          </div>
+          <Game
+            size={size}
+            grid={this.state.grid}
+            clues={this.state.clues}
+            frozen={false}
+            updateGrid={this.updateGrid.bind(this)}
+          />
+        </div>
+        <div className='compose--right'>
+          <h2> Right sidebar </h2>
+        </div>
+
+      </div>
     );
   }
 };
